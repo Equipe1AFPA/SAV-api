0a91ff6f19f7a0a83a0208618c6173176985e1ff
Nazgad <debiasif31@gmail.com>
2022-01-20 09:45:22 +0100

    modification de la mise en page et des informations présente

--- accueil_auth.php
+++ accueil_auth.php
@@ -13,28 +13,28 @@
 </body>
 </html>
 
+<!-- Lien de connexion bootstrap, php, jquery. -->
+
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
 
 <div class="wrapper fadeInDown">
   <div id="formContent">
-    <!-- Tabs Titles -->
 
-    <!-- Icon -->
+    <!-- Logo de l'entreprise -->
     <div class="fadeIn first">
       <img src="images/Menuiz_Man.png" id="icon" alt="User Icon" />
     </div>
 
-    <!-- Login Form -->
+    <!-- Formulaire d'identification -->
     <form>
       <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
       <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
       <input type="submit" class="fadeIn fourth" value="Log In">
     </form>
 
-    <!-- Remind Passowrd -->
+    <!-- Mot de passe oublié -->
     <div id="formFooter">
       <a class="underlineHover" href="#">Forgot Password?</a>
     </div>
--- historique_dossier.php
+++ historique_dossier.php
@@ -31,46 +31,20 @@
       <h1>Historique de gestion de dossier</h1>
       <div class="row-inline">
         <div class="row justify-content-start col-md-12 p-2">
-            <!-- <form action="#" method="get"> -->
                   <div class="input-group">
-                    <div class="col-md-2  p-2">
-                      <h5 class="select-title">N° dossier : </h5>
-                      <select class="form-select form-select-sm col-8" aria-label=".form-select-lg example">
-                          <option selected></option>
-                          <option value="1">One</option>
-                          <option value="2">Two</option>
-                          <option value="3">Three</option>
-                      </select>
+                    <div class="col-md-4 offset-md-1  p-2">
+                      <h5 class="select-title">N° dossier : MYDOSNUM </h5>
                     </div>
                     <div class="col-md-4 offset-md-1 p-2">
-                      <h5 class="select-title">Client : </h5>
-                      <select class="form-select form-select-sm col-10" aria-label=".form-select-lg example">
-                          <option selected></option>
-                          <option value="1">One</option>
-                          <option value="2">Two</option>
-                          <option value="3">Threeeeeeeeeeeeeeeeeeeeeeeeeeeqdqsdqdqdqsfqfqfqdqdqdqdqdqdqee</option>
-                      </select>
+                      <h5 class="select-title">Client : MONSIEURLECLIENT </h5>
                     </div>
-
-                    <div class="col-md-2  offset-md-3 p-2">
-                      <h5 class="select-title">Date : </h5>
-                      <select class="form-select form-select-sm col-10" aria-label=".form-select-lg example">
-                          <option selected></option>
-                          <option value="1">One</option>
-                          <option value="2">Two</option>
-                          <option value="3">Three</option>
-                      </select>
-                      </div>
-                  </div>
-            <!-- </form> -->
       </div>
       <table class="table table-striped">
         <thead>
           <tr>
-            <th>Dossier N°</th>
-            <th>Nom du  client</th>
-            <th>Opération éffectué</th>
-            <th>Date/heure</th>
+            <th class="col-md-2 p-2">Date/heure</th>
+            <th class="col-md-3 p-2">Type de dossier</th>
+            <th class="col-md-7 p-2">Opération éffectué</th>
           </tr>
         </thead>
         <tbody>
@@ -78,43 +52,36 @@
             <td>Ma2019-12</td>
             <td>Giraud</td>
             <td>Rome</td>
-            <td>3</td>
           </tr>
           <tr>
             <td>Av2019-1</td>
             <td>Durand</td>
             <td>Madrid</td>
-            <td>22</td>
           </tr>
           <tr>
             <td>Jt2019-36</td>
             <td>Joly</td>
             <td>Mexico</td>
-            <td>Out of stock</td>
           </tr>
           <tr>
             <td>Ju2019-36</td>
-            <td>Expédition partielle du/des produit (s)</td>
+            <td>Expédition  (s)</td>
             <td>Moscou</td>
-            <td>27</td>
           </tr>
           <tr>
             <td>Ju2019-36</td>
             <td>Amar</td>
             <td>Amsterdam </td>
-            <td>Out of stock </td>
           </tr>
           <tr>
             <td>Ju2019-36</td>
             <td>Shuber</td>
             <td>Paris</td>
-            <td>100</td>
           </tr>
           <tr>
             <td>Ju2019-36</td>
             <td>Wald</td>
             <td>New York</td>
-            <td>72</td>
           </tr>
         </tbody>
       </table>
